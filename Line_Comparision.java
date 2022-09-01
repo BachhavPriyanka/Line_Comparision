813e75bd613a6422dab3ef8edda17e4926985ef9
BachhavPriyanka <bachhavpriyanka09@gmail.com>
2022-09-01 11:56:47 +0530

    Checking whether lines are equal or not

--- Line_Comparision.java
+++ Line_Comparision.java
@@ -12,25 +12,31 @@ public class Line_Comparision {
 	}
 	
 public static void prob1(){
-	int x1,x2,y1,y2;
+    int x1, x2, y1, y2;
 
     double length;
+
     Scanner sc = new Scanner(System.in);
 
     System.out.println("enter x1 point");
-    x1=sc.nextInt();
+    x1 = sc.nextInt();
 
     System.out.println("enter y1 point");
-    y1=sc.nextInt();
+    y1 = sc.nextInt();
 
     System.out.println("enter x2point");
-    x2=sc.nextInt();
+    x2 = sc.nextInt();
 
     System.out.println("enter y2 point");
-    y2=sc.nextInt();
+    y2 = sc.nextInt();
 
-    length=Math.sqrt((x2-x1)^2 + (y2-y1)^2);
-    System.out.println("Length of Lines is : "+"("+x1+","+y1+"),"+"("+x2+","+y2+")===>"+length);
+    length = Math.sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
 
+
+    System.out.println("Length of Lines is : " + "(" + x1 + "," + y1 + ")," + "(" + x2 + "," + y2 + ")===>" + length);
+
+    System.out.println(" Checking Equality of Two Lengths:");
+    System.out.println(x1 + " == " + y1 + " : " + (x1 == y1));
+    System.out.println(x2 + " == " + y2 + " : " + (x2 == y2));
 }
 }
